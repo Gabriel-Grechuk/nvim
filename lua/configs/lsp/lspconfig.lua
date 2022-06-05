656da48eacb17f515856052acb2d46665076aeee
Gabriel Grechuk <gabrielgrechuk@outlook.com>
2022-06-05 14:53:54 -0300

    Adicnado suporte a lsp

---
+++ lua/configs/lsp/lspconfig.lua
@@ -0,0 +1,116 @@
+local nvim_lsp = require('lspconfig')
+local protocol = require'vim.lsp.protocol'
+
+local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+---- Mappings.
+local opts = { noremap=true, silent=true }
+
+---- See `:help vim.lsp.*` for documentation on any of the below functions
+vim.api.nvim_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+--buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+--vim.api.nvim_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+--vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+vim.api.nvim_set_keymap('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+--buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+--buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+--buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+--buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+--buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+--buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+--buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+----buf_set_keymap('n', '<C-j>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+--buf_set_keymap('n', '<S-C-j>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+--buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+vim.api.nvim_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+
+local cmp = require'cmp'
+
+local on_attach = function(client, bufnr)
+	protocol.SymbolKind = { }
+	protocol.CompletionItemKind = {
+		'', -- Text
+		'', -- Method
+		'', -- Function
+		'', -- Constructor
+		'', -- Field
+		'', -- Variable
+		'', -- Class
+		'ﰮ', -- Interface
+		'', -- Module
+		'', -- Property
+		'', -- Unit
+		'', -- Value
+		'', -- Enum
+		'', -- Keyword
+		'﬌', -- Snippet
+		'', -- Color
+		'', -- File
+		'', -- Reference
+		'', -- Folder
+		'', -- EnumMember
+		'', -- Constant
+		'', -- Struct
+		'', -- Event
+		'ﬦ', -- Operator
+		'', -- TypeParameter
+	}
+
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+	cmp.setup({
+  	snippet = {
+    	-- REQUIRED - you must specify a snippet engine
+    	expand = function(args)
+				vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+				--require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      	-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+				--vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    	end,
+  	},
+  	mapping = {
+    	['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-5), { 'i', 'c' }),
+    	['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(3), { 'i', 'c' }),
+    	['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    	['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    	['<C-e>'] = cmp.mapping({
+      	i = cmp.mapping.abort(),
+      	c = cmp.mapping.close(),
+    	}),
+    	['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  	},
+  	sources = cmp.config.sources({
+    	{ name = 'nvim_lsp' },
+    	{ name = 'vsnip' }, -- For vsnip users.
+    	-- { name = 'luasnip' }, -- For luasnip users.
+    	-- { name = 'ultisnips' }, -- For ultisnips users.
+    	-- { name = 'snippy' }, -- For snippy users.
+  	}, {
+    	{ name = 'buffer' },
+  	})
+	})
+
+	-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+	cmp.setup.cmdline('/', {
+  	sources = {
+    	{ name = 'buffer' }
+  	}
+	})
+end
+
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+--require'lspconfig'.rust_analyzer.setup{
+	--on_attach = on_attach
+--}
+
+require('lspconfig')['ccls'].setup {
+	on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = nvim_lsp.util.root_pattern('./')
+}
+
