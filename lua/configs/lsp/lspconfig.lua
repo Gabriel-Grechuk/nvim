c6b3001bbbe645ef2ea974d5b3d8d7d9d251cf18
Gabe <gabrielgrechuk@outlook.com>
2024-02-10 01:23:40 -0300

    Go config

--- lua/configs/lsp/lspconfig.lua
+++ lua/configs/lsp/lspconfig.lua
@@ -92,7 +92,7 @@ end
 -- Setup lspconfig.
 -- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local servers = { 'rls', 'ccls', 'tsserver', 'pyright' }
+local servers = { 'rls', 'ccls', 'tsserver', 'pyright', 'gopls' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
