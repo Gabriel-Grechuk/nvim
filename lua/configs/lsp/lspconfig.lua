619183b3608365c97118952a926b364f40cd9f4b
Gabriel-Grechuk <gabrielgrechuk@outlook.com>
2024-03-11 16:48:16 -0300

    Add Rust Config

--- lua/configs/lsp/lspconfig.lua
+++ lua/configs/lsp/lspconfig.lua
@@ -92,7 +92,8 @@ end
 -- Setup lspconfig.
 -- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local servers = { 'rls', 'ccls', 'tsserver', 'pyright', 'gopls' }
+local servers = { 'rls', 'ccls', 'tsserver', 'pyright', 'gopls', 'rust-analyzer' }
+
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
