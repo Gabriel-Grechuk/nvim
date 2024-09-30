b329d36e84961a4342950e69545a1cc6665ef516
Gabe <gabrielgrechuk@outlook.com>
2024-09-30 16:54:57 -0300

    Update lsp and rust tools

--- lua/configs/lsp/lspconfig.lua
+++ lua/configs/lsp/lspconfig.lua
@@ -92,7 +92,7 @@ end
 -- Setup lspconfig.
 -- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local servers = { 'rls', 'ccls', 'tsserver', 'pyright', 'gopls', 'rust-analyzer' }
+local servers = { 'ccls', 'ts_ls', 'pyright', 'gopls', 'rust_analyzer' }
 
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
--- lua/configs/plugins.lua
+++ lua/configs/plugins.lua
@@ -101,17 +101,8 @@ require('nvim-treesitter.configs').setup {
 --})
 
 local rt = require("rust-tools")
-
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
-})
+rt.setup({})
+rt.inlay_hints.enable()
 
 vim.cmd [[
   let g:neoformat_try_node_exe = 1
